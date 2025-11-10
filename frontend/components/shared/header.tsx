aa20f748fcceab61f7bb8b86029df8e64427dec1
Holiday <thelastofinusa@gmail.com>
2025-11-10 19:33:34 +0100

    feat: header completed

--- frontend/components/shared/header.tsx
+++ frontend/components/shared/header.tsx
@@ -6,14 +6,16 @@ import { SignInButton } from "./sign-in-button";
 import Image from "next/image";
 import { siteConfig } from "@/config/site.config";
 import Link from "next/link";
+import { Search } from "lucide-react";
+import { Input } from "../ui/input";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 left-0 w-full py-4">
+    <header className="sticky top-0 left-0 w-full py-2 md:py-4">
       <Wrapper size="lg" className="size-full">
         <nav className="flex items-center justify-between gap-10 py-2">
-          <div className="max-w-[230px] w-full ">
-            <Link href="/" className="flex items-center gap-2 w-max">
+          <div className="w-full max-w-[180px] lg:max-w-[230px]">
+            <Link href="/" className="flex w-max items-center gap-2">
               <Image
                 src="/favicon.svg"
                 alt={siteConfig.title}
@@ -21,14 +23,23 @@ export const Header = () => {
                 height={22}
                 priority
                 quality={100}
+                className="h-8 w-10 md:h-[22px] md:w-[52px]"
               />
-              <p className="font-bold text-lg">{siteConfig.title}</p>
+              <p className="text-base font-bold md:text-lg">
+                {siteConfig.title}
+              </p>
             </Link>
           </div>
-          {/* <div className="flex-1 flex items-center justify-center">
-            Input Field
-          </div> */}
-          <div className="max-w-[230px] w-full flex justify-end">
+          <div className="hidden flex-1 items-center justify-center md:flex">
+            <div className="relative h-14 w-full max-w-md lg:max-w-xl">
+              <Input
+                className="size-full rounded-full bg-white/15 pr-14 pl-6"
+                placeholder="Search for any service..."
+              />
+              <Search className="text-muted-foreground absolute top-1/2 right-5 size-6 -translate-y-1/2" />
+            </div>
+          </div>
+          <div className="flex w-full max-w-[180px] justify-end lg:max-w-[230px]">
             <SignInButton />
           </div>
         </nav>
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,5 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { createAvatar } from "@dicebear/core";
+import { pixelArtNeutral } from "@dicebear/collection";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -7,10 +9,25 @@ export function cn(...inputs: ClassValue[]) {
 
 export function assertValue<T>(
   v: T | undefined,
-  errorMessage?: string
+  errorMessage?: string,
 ): NonNullable<T> {
   if (v === undefined || v === null) {
     throw new Error(errorMessage ?? "Missing property");
   }
   return v;
+}
+
+export function genAvatar(address: string): string | null {
+  if (!address) return null;
+
+  return createAvatar(pixelArtNeutral, {
+    seed: address,
+  }).toDataUri();
+}
+
+export function formatAddr(str: string | undefined, n: number = 4): string {
+  if (!str) return "";
+  return str?.length > n
+    ? str.slice(0, 6) + "..." + str.slice(str.length - n)
+    : str;
 }
